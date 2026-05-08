af0852200fad9584cfe69c04653cb8ac332685b0
srinath39 <ashwinisrinath2580@gmail.com>
2026-05-09 00:19:54 +0530

    To intercept a specific method , not the entire methods in class. we can use of Custom Annotations

--- src/main/java/com/MyProject/Learn_Spring_AOP/Aspects/LoggingAspect.java
+++ src/main/java/com/MyProject/Learn_Spring_AOP/Aspects/LoggingAspect.java
@@ -12,28 +12,28 @@ import org.springframework.context.annotation.Configuration;
 public class LoggingAspect {
 
     private Logger logger= LoggerFactory.getLogger(getClass());
-//
-//    @Before("com.MyProject.Learn_Spring_AOP.CommonPointCutConfig.businessAndDataServicePointCutConfig()")  // when ?
-//    public void logMethodCallBeforeExecution(JoinPoint joinPoint){
-//        logger.info("Before execution of {}",joinPoint); // what?
-//    }
-//
-//    @After("com.MyProject.Learn_Spring_AOP.CommonPointCutConfig.businessAndDataServicePointCutConfig()")  // when ?
-//    public void logMethodCallAfterExecution(JoinPoint joinPoint){
-//        logger.info("After execution of {}",joinPoint); // what?
-//    }
-//
-//    @AfterThrowing(
-//            pointcut = "com.MyProject.Learn_Spring_AOP.CommonPointCutConfig.businessAndDataServicePointCutConfig()",
-//            throwing = "exception")  // when ?
-//    public void logMethodCallAfterExecution(JoinPoint joinPoint,Exception exception){
-//        logger.info("After throwing exception of method {} this is the exception {}",joinPoint,exception); // what?
-//    }
-//
-//    @AfterReturning(
-//            pointcut = "com.MyProject.Learn_Spring_AOP.CommonPointCutConfig.businessAndDataServicePointCutConfig()",
-//            returning = "returnValue")  // when ?
-//    public void logMethodCallAfterExecution(JoinPoint joinPoint,Object returnValue){
-//        logger.info("After Returning exception of method {} this is the returned value {}",joinPoint,returnValue); // what?
-//    }
+
+    @Before("com.MyProject.Learn_Spring_AOP.CommonPointCutConfig.businessAndDataServicePointCutConfig()")  // when ?
+    public void logMethodCallBeforeExecution(JoinPoint joinPoint){
+        logger.info("Before execution of {}",joinPoint); // what?
+    }
+
+    @After("com.MyProject.Learn_Spring_AOP.CommonPointCutConfig.businessAndDataServicePointCutConfig()")  // when ?
+    public void logMethodCallAfterExecution(JoinPoint joinPoint){
+        logger.info("After execution of {}",joinPoint); // what?
+    }
+
+    @AfterThrowing(
+            pointcut = "com.MyProject.Learn_Spring_AOP.CommonPointCutConfig.businessAndDataServicePointCutConfig()",
+            throwing = "exception")  // when ?
+    public void logMethodCallAfterExecution(JoinPoint joinPoint,Exception exception){
+        logger.info("After throwing exception of method {} this is the exception {}",joinPoint,exception); // what?
+    }
+
+    @AfterReturning(
+            pointcut = "com.MyProject.Learn_Spring_AOP.CommonPointCutConfig.businessAndDataServicePointCutConfig()",
+            returning = "returnValue")  // when ?
+    public void logMethodCallAfterExecution(JoinPoint joinPoint,Object returnValue){
+        logger.info("After Returning exception of method {} this is the returned value {}",joinPoint,returnValue); // what?
+    }
 }
